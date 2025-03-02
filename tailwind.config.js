51364410d18f3cdf82da0162fa3d7861b364d56a
jabbarrs <cabbarustemov@gmail.com>
2025-03-02 00:21:47 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./**/*.{htm,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        manrope: ["Manrope", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
