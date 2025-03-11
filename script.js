167100bfdfb28003cd8b7424502e99bd982b120e
jabbarrs <cabbarustemov@gmail.com>
2025-03-11 21:55:52 +0400

    some changes and swiper1

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
 const sidebar = document.querySelector('.sidebar');
+const down = document.getElementById('down');
+const header = document.getElementById('header');
+const arr = ['.az', '.com', '.net', '.info', '.biz', '.org', '.us', '.in', '.ws', '.mobi', '.tv', '.me', '.ru', '.de', '.com.az', '.co', '.site.az', '.sayt.az', '.ureb.com', '.org.az', '.info.az', '.biz.az', '.net.az'];
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
 
 function toggleSidebar() {
     sidebar.classList.toggle('active');
 }
+
+function toDown() {
+    scrollTo({
+        top: down.offsetTop - header.scrollHeight,
+        behavior: "smooth"
+    })
+}
+
+let kod = ''
+
+deyish()
+
+function deyish(){
+    arr.map(item => {kod+= `<option>${item}</option>`})
+    langs.innerHTML = kod
+}
+
+function changeBtn() {
+    
+}
+
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+});
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,22 @@
         manrope: ["Manrope", "sans-serif"],
       },
     },
+
+    screens: {
+      'bm': '290px',
+      'hd': '340px',
+      'md1': '398px',
+      'am': '550px',
+      'em': '592px',
+      'sm': '640px',
+      'md': '768px',
+      'td': '841px',
+      'tm': '968px',
+      'lg': '1024px',
+      'mlg': '1150px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
   },
   plugins: [],
 }
