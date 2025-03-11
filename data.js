50db9d575485ce6668b698fcff7786858b15e64e
jabbarrs <cabbarustemov@gmail.com>
2025-03-11 23:53:19 +0400

    functionality

---
+++ data.js
@@ -0,0 +1,158 @@
+const rangeData = [
+    {
+            "id": 1,
+            "name": "Bloq",
+            "price": 200,
+            "type": "website_addons"
+    },
+    {
+            "id": 2,
+            "name": "Üzv girişi /‍‍‍ Profillər",
+            "price": 400,
+            "type": "website_addons"
+    },
+    {
+            "id": 8,
+            "name": "Responsive",
+            "price": 100,
+            "type": "website_addons"
+    },
+    {
+            "id": 22,
+            "name": "Forum",
+            "price": 400,
+            "type": "website_addons"
+    },
+    {
+            "id": 23,
+            "name": "Mesajlaşma",
+            "price": 50,
+            "type": "website_addons"
+    },
+    {
+            "id": 24,
+            "name": "Canlı Söhbət",
+            "price": 50,
+            "type": "website_addons"
+    },
+    {
+            "id": 25,
+            "name": "Qalereya",
+            "price": 100,
+            "type": "website_addons"
+    },
+    {
+            "id": 26,
+            "name": "Əlaqə Forması",
+            "price": 100,
+            "type": "website_addons"
+    },
+    {
+            "id": 27,
+            "name": "Saytda Axtarış",
+            "price": 100,
+            "type": "website_addons"
+    },
+    {
+            "id": 28,
+            "name": "Xəritə Məlumatı / Geolocation",
+            "price": 100,
+            "type": "website_addons"
+    },
+    {
+            "id": 29,
+            "name": "Rezervasyon",
+            "price": 400,
+            "type": "website_addons"
+    },
+    {
+            "id": 30,
+            "name": "Sosial Media inteqrasiyası",
+            "price": 50,
+            "type": "website_addons"
+    },
+    {
+            "id": 31,
+            "name": "Analitika / İzləmə",
+            "price": 50,
+            "type": "website_addons"
+    },
+    {
+            "id": 36,
+            "name": "Admin Panel",
+            "price": 300,
+            "type": "website_addons"
+    }
+]
+
+const checkData = [
+    {
+            "id": 3,
+            "name": "Başlıqlar & Meta",
+            "price": 200,
+            "type": "seo_addons"
+    },
+    {
+            "id": 32,
+            "name": "Açar söz",
+            "price": 300,
+            "type": "seo_addons"
+    },
+    {
+            "id": 33,
+            "name": "+20 Qabaqcıl Texnika",
+            "price": 300,
+            "type": "seo_addons"
+    },
+    {
+            "id": 34,
+            "name": "Başlıq Teqləri",
+            "price": 100,
+            "type": "seo_addons"
+    },
+    {
+            "id": 35,
+            "name": "Sitemap",
+            "price": 100,
+            "type": "seo_addons"
+    }
+]
+
+const eccomerceData = [
+    {
+            "id": 38,
+            "name": "Ödəniş",
+            "price": 200,
+            "type": "ecommerce"
+    },
+    {
+            "id": 39,
+            "name": "Səbət",
+            "price": 500,
+            "type": "ecommerce"
+    },
+    {
+            "id": 40,
+            "name": "Məhsullar",
+            "price": 300,
+            "type": "ecommerce"
+    },
+    {
+            "id": 41,
+            "name": "Bəyəndiklərim",
+            "price": 300,
+            "type": "ecommerce"
+    },
+    {
+            "id": 42,
+            "name": "Filtr",
+            "price": 300,
+            "type": "ecommerce"
+    },
+    {
+            "id": 43,
+            "name": "Məhsuk Haqqında",
+            "price": 200,
+            "type": "ecommerce"
+    }
+]
--- script.js
+++ script.js
@@ -5,6 +5,29 @@ const arr = ['.az', '.com', '.net', '.info', '.biz', '.org', '.us', '.in', '.ws'
 const btn1 = document.getElementById('btn1');
 const btn2 = document.getElementById('btn2');
 
+const calc = document.getElementById('calc')
+const calcSearch = document.getElementById('calcSearch')
+const calcelect = document.getElementById('calcelect')
+const pages = document.getElementById('pages')
+const input1 = document.querySelector('#input1')
+const input2 = document.querySelector('#input2')
+const input3 = document.getElementById('input3')
+const prices = document.getElementById('price')
+const prices4 = document.getElementById('prices4')    
+const prices2 = document.getElementById('prices2')    
+const prices3 = document.getElementById('prices3')    
+
+const stat = document.getElementById('stat')
+const stat1 = document.getElementById('stat1')
+const stat2 = document.getElementById('stat2')
+const stat3 = document.getElementById('stat3')
+const stat4 = document.getElementById('stat4')
+const totalpr = document.getElementById('totalpr')
+let ntc = 0
+let range = 0
+let range1 = 0
+let range2 = 0
+
 function toggleSidebar() {
     sidebar.classList.toggle('active');
 }
@@ -18,9 +41,9 @@ function toDown() {
 
 let kod = ''
 
-deyish()
+change()
 
-function deyish(){
+function change(){
     arr.map(item => {kod+= `<option>${item}</option>`})
     langs.innerHTML = kod
 }
@@ -64,3 +87,82 @@ let swiper1 = new Swiper(".slider2", {
 });
 
 
+
+function calculator(){
+    rangeData.map(item => {
+        calc.innerHTML += `
+                        <label class="relative flex cursor-pointer justify-between items-center">
+                            <span class="text-sm font-medium text-gray-900">${item.name}</span>
+                            <input onclick="changePrice(this, ${item.price})" type="checkbox" id="${item.id}">
+                        </label>
+        `
+    })
+
+    checkData.map(item => {
+        calcSearch.innerHTML += `
+                           <div class="flex items-center mr-2 cursor-pointer">
+                                <input onclick="changePrice2(this, ${item.price})" type="checkbox" id="${item.id}" />
+                              <p class="p-2">${item.name}</p>
+                            </div> 
+                        `
+    })
+    eccomerceData.map(item => {
+        calcelect.innerHTML += `
+                        <label class="relative flex cursor-pointer justify-between items-center">
+                            <span class="text-sm font-medium text-gray-900">${item.name}</span>
+                            <input onclick="changePrice3(this, ${item.price})" type="checkbox" id="${item.id}"> 
+                        </label> 
+                        `
+    })
+   }
+   calculator()
+   let inputs = 0
+
+   function change1() {
+    const pageCount = +input1.value;
+    const complexity = +input2.value;
+    const total = (pageCount * 100) * complexity;
+    pages.innerHTML = `${pageCount} səhifə / ${total} ₼`;
+    stat.innerHTML = `₼ ${total}`;
+    totalCalc();
+}
+
+    change1()
+
+    
+    function changePrice(elem, price) {
+        range += elem.checked ? price : -price;
+        prices.innerHTML = `₼ ${range}`;
+        stat1.innerHTML = `₼ ${range}`;
+        totalCalc();
+    }
+    
+    function changePrice2(elem, price) {
+        range1 += elem.checked ? price : -price;
+        prices2.innerHTML = `₼ ${range1}`;
+        stat2.innerHTML = `₼ ${range1}`;
+        totalCalc();
+    }
+    
+    function changePrice3(elem, price) {
+        range2 += elem.checked ? price : -price;
+        prices3.innerHTML = `₼ ${range2}`;
+        stat3.innerHTML = `₼ ${range2}`;
+        totalCalc();
+    }
+
+    function logoinp() {
+        const logoPrice = input3.value * 100;
+        prices4.innerHTML = `₼ ${logoPrice}`;
+        stat4.innerHTML = `₼ ${logoPrice}`;
+        totalCalc();
+    }
+
+    function totalCalc() {
+        const pageCount = +input1.value;
+        const complexity = +input2.value;
+        const totalPages = (pageCount * 100) * complexity;
+        const total = range + range1 + range2 + (input3.value * 100) + totalPages;
+        totalpr.innerHTML = total < 450 ? `₼ 450` : `₼ ${total}`;
+    }
+    totalCalc()
